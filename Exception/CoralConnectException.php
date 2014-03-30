9fe5ea8c583d09cbe1bf67336bc8dd1f20cb2abf
Frantisek Troster <frantisek.troster@inventic.eu>
2014-03-30 23:29:38 +0200

    Fixed file header

--- Exception/CoralConnectException.php
+++ Exception/CoralConnectException.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- * This file is part of the Symfony package.
+ * This file is part of the Coral package.
  *
- * (c) Fabien Potencier <fabien@symfony.com>
+ * (c) Frantisek Troster <frantisek.troster@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
